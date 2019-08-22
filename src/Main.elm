c8a30c61b7cb84a229f9fa72b6408fdab608c8e3
sairim <spiri@protonmail.com>
2019-08-23 00:37:46 +0300

    make progress on view

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,9 @@ import Browser
 import Browser.Dom as Dom
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Lazy exposing (lazy, lazy2)
+import Task
 
 main : Program (Maybe Model) Model Msg
 main =
@@ -56,10 +59,7 @@ type Msg
   | UpdateField String
   | ConjugateField String
 
-type Verb
-  = GroupA String
-  | GroupB String
-  | GroupG String
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -74,44 +74,65 @@ update msg model =
       )
 
     
-    ConjugateField verb ->
-      case verb of
-
-        GroupA str ->
-          ( { model | conjugation =
-            { str
-            , String.dropRight 1 ++ "εις"
-            , String.dropRight 1 ++ "ει"
-            , String.dropRight 1 ++ "ουμε"
-            , String.dropRight 1 ++ "ετε"
-            , String.dropRight 1 ++ "ουνε"
-            } }
+    ConjugateField str ->
+          ( { model 
+              | field = ""
+              , conjugation =
+                if String.endsWith "άω" str then
+                  { aEnikos = str
+                  , bEnikos = String.dropRight 1 str ++ "ς"
+                  , gEnikos = String.dropRight 1 str ++ "ει"
+                  , aPlithintikos = String.dropRight 1 str ++ "με"
+                  , bPlithintikos = String.dropRight 1 str ++ "τε"
+                  , gPlithintikos = String.dropRight 1 str ++ "νε"
+                  } 
+                
+                else if String.endsWith "ώ" str then
+                  { aEnikos = str
+                  , bEnikos = String.dropRight 1 str ++ "είς"
+                  , gEnikos = String.dropRight 1 str ++ "εί"
+                  , aPlithintikos = String.dropRight 1 str ++ "ούμε"
+                  , bPlithintikos = String.dropRight 1 str ++ "είτε"
+                  , gPlithintikos = String.dropRight 1 str ++ "ούνε"
+                  }                 
+                else
+                  { aEnikos = str
+                  , bEnikos = String.dropRight 1 ++ "εις"
+                  , gEnikos = String.dropRight 1 ++ "ει"
+                  , aPlithintikos = String.dropRight 1 ++ "ουμε"
+                  , bPlithintikos = String.dropRight 1 ++ "ετε"
+                  , gPlithintikos = String.dropRight 1 ++ "ουνε"
+                  } 
+            }
             , Cmd.none
           )
-
-
-        GroupB str ->
-          ( { model | conjugation = 
-            { str
-            , String.dropRight 1 str ++ "ς"
-            , String.dropRight 1 str ++ "ει"
-            , String.dropRight 1 str ++ "με"
-            , String.dropRight 1 str ++ "τε"
-            , String.dropRight 1 str ++ "νε"
-            } }
-            , Cmd.none
-          })
-
-
-        GroupG str ->
-          ( { model | conjugation = 
-            { str
-            , String.dropRight 1 str ++ "είς"
-            , String.dropRight 1 str ++ "εί"
-            , String.dropRight 1 str ++ "ούμε"
-            , String.dropRight 1 str ++ "είτε"
-            , String.dropRight 1 str ++ "ούνε"
-            } }
-            , Cmd.none
-          })
         
+
+-- VIEW
+
+view : Model -> Html Msg
+view model = 
+  div [] 
+    [ section
+      [ lazy viewInput model.field 
+      , lazy2 viewConjugation model.visibility model.conjugation
+      ]
+    ]
+
+viewInput : String -> Html Msg
+viewInput verb =
+  header
+    [ h1 [] [ text "Greek Conjugator" ]
+    , input
+      [ placeholder "What verb would you like to conjugate?"
+      , autofocus true
+      , value verb
+      , name "verb"
+      , onInput UpdateField
+      , onEnter ConjugateField
+      ]
+      []
+    ]
+
+
+  
